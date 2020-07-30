6d7ab82ef77f1be71c9561aaf3fd30e97a6585aa
Julia Gu <juliagu808@gmail.com>
2020-07-30 20:10:22 +0200

    c++ complete

--- CPPdemo/summerAssignment/summerAssignment/Source.cpp
+++ CPPdemo/summerAssignment/summerAssignment/Source.cpp
@@ -6,18 +6,42 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "swedish");
+
+	cout << "Uppgift 1 \n";
 	string text;
 	cout << "skriva text: \n";
-	cin >> text;
+	getline(cin, text);
 
 	if (text == "Stefan") {
-		cout << "Hej Stefan";
+		cout << "Hej Stefan\n";
 	}
 	else
 	{
-		cout << "Jaha du är inte Stefan";
+		cout << "Jaha du är inte Stefan\n";
 	}
 
+	cout << "Uppgift 2 \n";
+	float total = 0;
+	string input;
+	float num = 0;
+	int index = 1;
+	while (true) {
+		cout << "Nr: " << index << " -> skriva ett tal: " << endl;
+		cin >> input;
+		stringstream s(input);
+		if (!(s >> num) || !s.eof()) {
+			cout << "fel num " << input << " !" << endl;
+		}
+		else {
+			s >> num;
+			index = index + 1;
+			total = total + num;
+		}
+		if (index == 11) {
+			break;
+		}
+	}
+	cout << "summan: " << total << endl;
 
 	return 0;
 }
